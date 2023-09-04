d02b7993607de732e9ca3bad78d670b281663ddf
Drake Du <112792477+drakedu@users.noreply.github.com>
2023-09-04 14:35:55 +0000

    Drake transformed image.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -27,7 +27,7 @@ const imageAltText = "This image is of an adult man wearing a mask and standing 
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <img className="background" src={image} alt="" />
+      <img className="background" src={image} alt="" style={{ transform: `rotate(180deg) translate(50%)` }} />
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem", color: "white" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
